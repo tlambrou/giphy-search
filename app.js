808f8e3a92dcfb06d5d989041a23211c0dd50581
tlambrou <lambrou@gmail.com>
2017-10-08 21:10:46 -0700

    Added app.js, some views, and a gitignore

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+app.get('/', function (req, res) {
+  console.log(req.query)
+  res.render('home')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
